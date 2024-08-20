5f21f896280037aaf332e863f4b68321354580f8
yilmaz-busra <b.busraayilmaz@gmail.com>
2024-08-20 22:24:31 +0300

    Add lazy loading for course cards on home page

--- src/Pages/Hom/Home.js
+++ src/Pages/Hom/Home.js
@@ -1,7 +1,22 @@
 import React from "react";
+import { courses } from "../../courses";
+import Card from "../../Components/Card";
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div className="container-card">
+      <div className="course-card">
+        {courses.slice(0, 3).map((course) => (
+          <Card
+            key={course.id}
+            title={course.title}
+            date={course.date}
+            preinformation={course.preinformation}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default Home;
