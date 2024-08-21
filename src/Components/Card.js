66dc8dfd7e5239e1f0ca49cb8bc00837e0639120
yilmaz-busra <b.busraayilmaz@gmail.com>
2024-08-21 21:43:22 +0300

    imported css file & added and changed props

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,12 +1,18 @@
 import React from "react";
+import "./card.css";
 
-function Card({ title, date, description }) {
+function Card({ title, preinformation, date, buttonText, image }) {
   return (
     <div className="course-card">
-      <h3>{title}</h3>
-      <p>{description}</p>
-      <span>{date}</span>
-      <button>Bekleyen</button>
+      <div className="card-header">
+        <img src={`/${image}`} alt="workshop" />
+        <h3>{title}</h3>
+        <p>{preinformation}</p>
+      </div>
+      <div className="card-footer">
+        <span>Son Başvuru: {date}</span>
+        <button>{buttonText}</button>
+      </div>
     </div>
   );
 }
