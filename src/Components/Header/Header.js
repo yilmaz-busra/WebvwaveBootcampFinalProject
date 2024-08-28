8cd6cde8908a07076a61a22ac1d89979610eded7
Busra Yilmaz <b.busraayilmaz@gmail.com>
2024-08-29 01:48:50 +0300

    created header component

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,32 @@
+import React from "react";
+import Slider from "react-slick";
+import "./styles.css";
+function Header() {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+  return (
+    <div className="slider-container">
+      <Slider {...settings}>
+        <div>
+          <img src="carousel.png" alt="carousel2" />
+        </div>
+        <div>
+          <img src="carousel2.png" alt="carousel2" />
+        </div>
+        <div>
+          <img src="carousel3.png" alt="carousel3" />
+        </div>
+        <div>
+          <img src="carousel4.png" alt="carousel4" />
+        </div>
+      </Slider>
+    </div>
+  );
+}
+
+export default Header;
