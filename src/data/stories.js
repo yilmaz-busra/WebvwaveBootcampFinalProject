0f87087531b7149295918ada2e12312cd90f6701
Busra Yilmaz <b.busraayilmaz@gmail.com>
2024-08-28 13:46:40 +0300

    created stories array

---
+++ src/data/stories.js
@@ -0,0 +1,21 @@
+export const stories = [
+  {
+    id: 1,
+    name: "Tugay Boz",
+    title: "Tech Recruitment Bootcamp",
+    feedback:
+      "Tek kelimeyle harikaydı. Şu da eklenebilir dediğim tek nokta olmadı...",
+  },
+  {
+    id: 2,
+    name: "Serhat Dokumacı",
+    title: "Sigortam.net Software QA Bootcamp",
+    feedback: "Sigortam.net QA Bootcamp'ta keyifli altı hafta geçirdim...",
+  },
+  {
+    id: 3,
+    name: "Serhat Dokumacı",
+    title: "Sigortam.net Software QA Bootcamp",
+    feedback: "Sigortam.net QA Bootcamp'ta keyifli altı hafta geçirdim...",
+  },
+];
