cfc94710fd4d403812dfbaa39fd5e6d05af8066c
Busra Yilmaz <b.busraayilmaz@gmail.com>
2024-08-28 13:45:41 +0300

    created card & props imple.

---
+++ src/Components/StoryCard/StoryCard.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "./styles.css";
+
+import Card from "react-bootstrap/Card";
+import { stories } from "../../data/stories";
+
+const StoryCard = ({ name, title, feedback }) => {
+  return (
+    <div className="testimonials-section">
+      .{" "}
+      <div className="testimonial">
+        <Card style={{ width: "18rem ", height: "18rem" }}>
+          <Card.Img variant="top" src="holder.js/100px180" />
+          <Card.Body>
+            <Card.Text>{feedback}</Card.Text>
+            <Card.Title>{name}</Card.Title>
+            <Card.Title>{title}</Card.Title>
+          </Card.Body>
+        </Card>
+      </div>
+    </div>
+  );
+};
+
+export default StoryCard;
