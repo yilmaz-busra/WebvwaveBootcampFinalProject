42b728778f944810047662286cb3e9b9eed6a855
Busra Yilmaz <b.busraayilmaz@gmail.com>
2024-08-28 16:03:36 +0300

    stories array editted

--- src/data/stories.js
+++ src/data/stories.js
@@ -1,21 +1,34 @@
 export const stories = [
   {
     id: 1,
-    name: "Tugay Boz",
-    title: "Tech Recruitment Bootcamp",
+    name: "Serhat Dokumacı",
+    image: "ogrenci-e.png",
+    title: "Sigortam.net Software QA Bootcamp",
     feedback:
-      "Tek kelimeyle harikaydı. Şu da eklenebilir dediğim tek nokta olmadı...",
+      "Sigortam.net QA Bootcamp'i ile dolu dolu ve çok keyifli altı hafta geçirdim. Bootcamp süreci meslek hayatımın başlangıcında bana yön veren ve istediğim alanda çalışmama vesile olan güzel bir süreç oldu. Bu süreçte çok eğlendiğim gibi yeni arkadaşlıklar da edindim ve kendimi de çok iyi bir biçimde geliştirdim. Eğitmenimiz, Sigortam.net ekibi ve Techcareer.net ekibi bana her alanda destek verip, tüm süreçlerde emin adımlarla ilerlememe yardımcı oldular. Bootcamp süreci sonunda Sigortam.net ailesine QA Specialist olarak katıldım. Bu imkânı bize sağladığı için Techcareer.net ailesine ve emeği geçen herkese çok teşekkür ederim, iyi ki katılmışım.",
   },
   {
     id: 2,
-    name: "Serhat Dokumacı",
+    name: "Sema Vargez",
+    image: "ogrenci-k.png",
     title: "Sigortam.net Software QA Bootcamp",
-    feedback: "Sigortam.net QA Bootcamp'ta keyifli altı hafta geçirdim...",
+    feedback:
+      "Techcareer.net sayesinde bir parçası olduğum Test Engineering Bootcamp süreci boyunca, techcareer.net ailesi ve eğitmenim iş hayatına sağlam adımlarla adım atmam için oldukça destekleyiciydi. Gerek bootcamp süreçleri gerek ise bootcamp sonrası ilgilerini eksik etmedikleri için techcareer.net ailesine ve eğitmenime teşekkür ederim. Halen bu ailenin bir parçası olduğum için oldukça mutluyum. Eminim ki techcareer.net ailesi vermiş olduğu bootcamplerle birlikte birçok insanın hayatına dokunmaya devam edecek.",
   },
   {
     id: 3,
-    name: "Serhat Dokumacı",
-    title: "Sigortam.net Software QA Bootcamp",
-    feedback: "Sigortam.net QA Bootcamp'ta keyifli altı hafta geçirdim...",
+    name: "Baran Emre Bolak",
+    image: "ogrenci-e2.png",
+    title: "Hyper Company Front-End Angular Bootcamp",
+    feedback:
+      "Eğitim süreci boyunca edindiğim bilgiler, becerilerimi geliştirmeme ve gerçek dünya projelerinde daha etkili bir şekilde çalışmama olanak tanıdı. Öncelikle, eğitmenlerin uzmanlığı ve öğretme tarzları beni etkileyerek, karmaşık Angular konseptlerini anlamamı sağladı. Derslerin interaktif ve uygulamalı olması, teorik bilgileri hemen pratiğe dökmeme fırsat tanıdı. Ayrıca, gerçek projelerde çalışma şansı bulmak, öğrenilenleri günlük iş süreçlerine entegre etmemde büyük bir rol oynadı. Eğitim sürecinin en değerli yanlarından biri, sadece Angular'ı değil, aynı zamanda genel web geliştirme prensiplerini de öğrenmek oldu. Programın organizasyonu ve içeriği, katılımcılara kapsamlı bir bakış açısı kazandırdı ve her seviyeden geliştiriciye hitap etti. Ayrıca, eğitim sonrasında sağlanan destek ve kaynaklar, kendi projelerimi geliştirmemde bana rehberlik etti.",
+  },
+  {
+    id: 3,
+    name: "Tugce Altın",
+    image: "ogrenci-k2.png",
+    title: "Hyper Company Front-End Angular Bootcamp",
+    feedback:
+      "Biyologluktan Yazılıma... Profesyonel kariyer hayatıma bir biyolog olarak başladım. Bu süreç pekte istediğim gibi gitmedi kariyer hedefimde istediğim yerden çok uzaktaydım. Yazılım benim için ulaşmak istediğim bir hayaldi ve 'Bir biyologtan yazılımcı olur mu?' sorusu beni engelliyordu. Şimdi değilse ne zaman dedim ve 2022 başı itibariyle sıkı bir tempoyla yazılım öğrenmeye Java ile başladım. 7 Aylık yoğun bir çalışmadan sonra Sigortam.net tarafından sağlanan Test Automation Bootcamp'ine kabul edildim. Yoğun süren 2 aylık bootcamp sürecinde Python ve Java ile test automation geliştirmelerinde bulundum. Başarılı bir şekilde tamamladığım bootcamp sonucunda bir diğer iLab firması olan Kariyer.Net bünyesinde yazılım dünyasına profesyonel bir giriş yaptım. Bu sürece başlayabilmeme fırsat tanıyan techcareer.net, sigortam.net ailesine ve bootcamp sürecimde büyük emeği geçen eğitmenim ve aynı zaman da Test Team Manager'ım olan Tuğçe Alphan'a kucak dolusu teşekkürlerimi sunarım.",
   },
 ];
