e6151b3b4af093cc4b05a0b7f7b20bbc39ca5afc
Busra Yilmaz <b.busraayilmaz@gmail.com>
2024-08-30 01:42:20 +0300

    Added education courses data array

--- src/data/courses.js
+++ src/data/courses.js
@@ -25,3 +25,82 @@ export const courses = [
       "Node.js Workshop Backend geliştirme dünyasında kendinizi geliştirmeye ve web uygulamalarınız için güçlü sunucu tarafı çözümleri üretmeye ne dersiniz? 🌐💻 Node.js kullanarak yüksek performanslı ve ölçeklenebilir web uygulamaları oluşturmayı öğrenin. Bu 10 saatlik workshop ile Node.js'in temel kavramlarını, modüllerini ve uygulama geliştirme süreçlerini öğreneceksiniz. Hemen başvurun ve Node.js ile backend geliştirme yolculuğunuza başlayın!",
   },
 ];
+
+export const courseseducation = [
+  {
+    title: "Java SE Eğitimi",
+    instructor: "Hamit Mızrak",
+    description:
+      "Java SE, geniş kapsamlı bir programlama dili olan Java'nın standart sürümüdür ve yazılım geliştirme alanında yaygın olarak...",
+    tags: ["Java SE", "Programlama", "OOP"],
+    duration: "19 Saat",
+    status: "Var",
+  },
+  {
+    title: "Angular Eğitimi",
+    instructor: "Hamit Mızrak",
+    description:
+      "Angular, web uygulamalarını geliştirmek için kullanılan güçlü bir framework'tür. Bu eğitimde, Angular'ın temel prensiplerini...",
+    tags: ["Angular", "SPA", "Web Geliştirme"],
+    duration: "12 Saat",
+    status: "Var",
+  },
+  {
+    title: "Git ve GitHub Eğitimi",
+    instructor: "Ahmet Kaya",
+    description:
+      "Sadece bir kurs ile Git ve GitHub konusunda uzmanlaşabilir ve projelerinizi yöneten binlerce kişiden biri...",
+    tags: ["GitHub", "BT ve Yazılım", "Git"],
+    duration: "3 Saat",
+    status: "Var",
+  },
+  {
+    title: "React ve Redux Eğitimi",
+    instructor: "Ayşe Yılmaz",
+    description:
+      "React ve Redux, modern web uygulamaları geliştirmek için kullanılan popüler JavaScript kütüphanesi ve durum yönetim aracıdır...",
+    tags: ["React", "Redux", "JavaScript", "Frontend"],
+    duration: "15 Saat",
+    status: "Var",
+  },
+
+  {
+    title: "Python ile Veri Bilimi",
+    instructor: "Mehmet Kaya",
+    description:
+      "Python dili, veri analizi, görselleştirme ve makine öğrenimi alanlarında geniş bir yelpazede kullanım imkanı sunar...",
+    tags: ["Python", "Veri Bilimi", "Makine Öğrenimi"],
+    duration: "25 Saat",
+    status: "Var",
+  },
+
+  {
+    title: "Web Geliştirme 101",
+    instructor: "Zeynep Tekin",
+    description:
+      "HTML, CSS ve JavaScript ile web geliştirme dünyasına adım atın. Bu eğitim, temel web teknolojilerini öğrenmenize yardımcı olacak...",
+    tags: ["HTML", "CSS", "JavaScript", "Web Geliştirme"],
+    duration: "12 Saat",
+    status: "Var",
+  },
+
+  {
+    title: "Agile ve Scrum Eğitimi",
+    instructor: "Ali Çetin",
+    description:
+      "Agile ve Scrum metodolojileri, yazılım geliştirme süreçlerini daha verimli hale getirmek için kullanılan popüler yöntemlerdir...",
+    tags: ["Agile", "Scrum", "Proje Yönetimi"],
+    duration: "8 Saat",
+    status: "Var",
+  },
+
+  {
+    title: "Veritabanı Yönetimi ve SQL",
+    instructor: "Elif Şahin",
+    description:
+      "Veritabanı yönetimi ve SQL ile veri tabanlarının nasıl çalıştığını ve veri sorgulama tekniklerini öğrenin...",
+    tags: ["SQL", "Veritabanı", "Veri Yönetimi"],
+    duration: "10 Saat",
+    status: "Var",
+  },
+];
