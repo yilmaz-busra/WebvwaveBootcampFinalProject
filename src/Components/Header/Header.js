f51f64daac313c7dfa44de882e59cbfbe6a9bf5c
yilmaz-busra <b.busraayilmaz@gmail.com>
2024-08-31 04:38:04 +0300

    chore: Update slider CSS class names for consistency

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -14,38 +14,38 @@ function Header() {
   return (
     <div className="slider-container">
       <Slider {...settings}>
-        <div className="slidercontent">
-          <div className="sliderdoticon">
+        <div className="slider-content">
+          <div className="slider-doticon">
             <Shapedot />
           </div>
-          <div className="sliderkesikicon">
+          <div className="slider-kesikicon">
             <Shapekesik />
           </div>
           <img src="carousel.png" alt="carousel2" />
         </div>
-        <div className="slidercontent">
-          <div className="sliderdoticon">
+        <div className="slider-content">
+          <div className="slider-doticon">
             <Shapedot />
           </div>
-          <div className="sliderkesikicon">
+          <div className="slider-kesikicon">
             <Shapekesik />
           </div>
           <img src="carousel2.png" alt="carousel2" />
         </div>
-        <div className="slidercontent">
-          <div className="sliderdoticon">
+        <div className="slider-content">
+          <div className="slider-doticon">
             <Shapedot />
           </div>
-          <div className="sliderkesikicon">
+          <div className="slider-kesikicon">
             <Shapekesik />
           </div>
           <img src="carousel3.png" alt="carousel3" />
         </div>
-        <div className="slidercontent">
-          <div className="sliderdoticon">
+        <div className="slider-content">
+          <div className="slide-rdoticon">
             <Shapedot />
           </div>
-          <div className="sliderkesikicon">
+          <div className="slider-kesikicon">
             <Shapekesik />
           </div>
           <img src="carousel4.png" alt="carousel4" />
