b9ca10ba54ded53593f9521ba1f36107ad2d4176
yilmaz-busra <b.busraayilmaz@gmail.com>
2024-08-31 04:30:43 +0300

    navbar changed

--- src/App.js
+++ src/App.js
@@ -3,24 +3,21 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./Pages/Home/Home";
 import Education from "./Pages/Education/Education";
 import JobPost from "./Pages/JobPost/JobPost";
-import Navbar from "./Components/Navbar";
-import Events from "./Pages/Events/Events";
-import Navbar1 from "./Components/Navbar/Navbar";
+
 import Company from "./Pages/Companies/Company";
+import NavHeader from "./Components/NavHeader/NavHeader";
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Navbar1 />
-        {/* <Navbar /> */}
+        <NavHeader />
+
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/education" element={<Education />} />
           <Route path="/jobpost" element={<JobPost />} />
           <Route path="/companies" element={<Company />} />
-
-          <Route path="/events" element={<Events />} />
         </Routes>
       </Router>
     </div>
