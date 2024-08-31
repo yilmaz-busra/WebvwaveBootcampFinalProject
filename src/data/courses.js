5976044b4b7010c0cef93d41da21476ca21f99ee
yilmaz-busra <b.busraayilmaz@gmail.com>
2024-08-31 11:52:25 +0300

    feat: Added new workshops to course data

--- src/data/courses.js
+++ src/data/courses.js
@@ -26,6 +26,60 @@ export const courses = [
     description:
       "Node.js Workshop Backend geliştirme dünyasında kendinizi geliştirmeye ve web uygulamalarınız için güçlü sunucu tarafı çözümleri üretmeye ne dersiniz? 🌐💻 Node.js kullanarak yüksek performanslı ve ölçeklenebilir web uygulamaları oluşturmayı öğrenin. Bu 10 saatlik workshop ile Node.js'in temel kavramlarını, modüllerini ve uygulama geliştirme süreçlerini öğreneceksiniz. Hemen başvurun ve Node.js ile backend geliştirme yolculuğunuza başlayın!",
   },
+  {
+    id: 4,
+    title: "React Native Workshop",
+    date: "28.08.2024",
+    image: "workshop.png",
+    preinformation: "Ücretsiz Geleceğin...",
+    description:
+      "React Native Workshop’a Katılmaya Hazır mısın? 📱💻Mobil uygulama geliştirme dünyasına adım atarak hem iOS hem de Android platformlarında çalışacak uygulamalar yaratmayı öğrenmeye ne dersin? React Native kullanarak uygulama geliştirme sürecini hızlandıracak ve platformlar arası işbirliğini artıracaksın! Bu 10 saatlik workshop ile React Native'in temel kavramlarını, bileşen yapısını, stil yönetimini ve uygulama geliştirmenin püf noktalarını öğreneceksin. Hemen kaydol ve React Native ile mobil uygulama geliştirme yolculuğuna başla!",
+  },
+  {
+    id: 5,
+    title: "Advanced SQL Pro Workshop",
+    date: "28.08.2024",
+    image: "workshop.png",
+    preinformation: "Ücretsiz Geleceğin...",
+    description:
+      "Advanced SQL Pro Workshop, toplam 12 saat sürecek eğitim tam sana göre 🚀💻 Bu Workshop'ta, gelişmiş SQL sorgulama tekniklerini ve veri tabanı yönetim sistemlerinin derinliklerini öğrenme fırsatı bulacaksın. Aynı zamanda, gerçek dünya projeleri üzerinde çalışarak, öğrendiğin bilgileri pekiştirecek ve pratik deneyim kazanacaksın. SQL dünyasını derinlemesine kavramak ve profesyonel veri tabanı projelerinde kendine güvenle yer almak için bu fırsatı kaçırma!",
+  },
+  {
+    id: 6,
+    title: "Node.js Workshop",
+    date: "28.08.2024",
+    image: "workshop.png",
+    preinformation: "Ücretsiz Geleceğin...",
+    description:
+      "Node.js Workshop Backend geliştirme dünyasında kendinizi geliştirmeye ve web uygulamalarınız için güçlü sunucu tarafı çözümleri üretmeye ne dersiniz? 🌐💻 Node.js kullanarak yüksek performanslı ve ölçeklenebilir web uygulamaları oluşturmayı öğrenin. Bu 10 saatlik workshop ile Node.js'in temel kavramlarını, modüllerini ve uygulama geliştirme süreçlerini öğreneceksiniz. Hemen başvurun ve Node.js ile backend geliştirme yolculuğunuza başlayın!",
+  },
+  {
+    id: 7,
+    title: "React Native Workshop",
+    date: "28.08.2024",
+    image: "workshop.png",
+    preinformation: "Ücretsiz Geleceğin...",
+    description:
+      "React Native Workshop’a Katılmaya Hazır mısın? 📱💻Mobil uygulama geliştirme dünyasına adım atarak hem iOS hem de Android platformlarında çalışacak uygulamalar yaratmayı öğrenmeye ne dersin? React Native kullanarak uygulama geliştirme sürecini hızlandıracak ve platformlar arası işbirliğini artıracaksın! Bu 10 saatlik workshop ile React Native'in temel kavramlarını, bileşen yapısını, stil yönetimini ve uygulama geliştirmenin püf noktalarını öğreneceksin. Hemen kaydol ve React Native ile mobil uygulama geliştirme yolculuğuna başla!",
+  },
+  {
+    id: 8,
+    title: "Advanced SQL Pro Workshop",
+    date: "28.08.2024",
+    image: "workshop.png",
+    preinformation: "Ücretsiz Geleceğin...",
+    description:
+      "Advanced SQL Pro Workshop, toplam 12 saat sürecek eğitim tam sana göre 🚀💻 Bu Workshop'ta, gelişmiş SQL sorgulama tekniklerini ve veri tabanı yönetim sistemlerinin derinliklerini öğrenme fırsatı bulacaksın. Aynı zamanda, gerçek dünya projeleri üzerinde çalışarak, öğrendiğin bilgileri pekiştirecek ve pratik deneyim kazanacaksın. SQL dünyasını derinlemesine kavramak ve profesyonel veri tabanı projelerinde kendine güvenle yer almak için bu fırsatı kaçırma!",
+  },
+  {
+    id: 9,
+    title: "Node.js Workshop",
+    date: "28.08.2024",
+    image: "workshop.png",
+    preinformation: "Ücretsiz Geleceğin...",
+    description:
+      "Node.js Workshop Backend geliştirme dünyasında kendinizi geliştirmeye ve web uygulamalarınız için güçlü sunucu tarafı çözümleri üretmeye ne dersiniz? 🌐💻 Node.js kullanarak yüksek performanslı ve ölçeklenebilir web uygulamaları oluşturmayı öğrenin. Bu 10 saatlik workshop ile Node.js'in temel kavramlarını, modüllerini ve uygulama geliştirme süreçlerini öğreneceksiniz. Hemen başvurun ve Node.js ile backend geliştirme yolculuğunuza başlayın!",
+  },
 ];
 
 export const courseseducation = [
